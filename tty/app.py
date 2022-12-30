1047802e5948dddd853ea5e4086f22919537616d
smbunty <118590024+smbunty@users.noreply.github.com>
2022-12-30 13:29:04 +0530

    create def for scraping
    def fun. ussed for web scraping like twitter

---
+++ tty/app.py
@@ -0,0 +1,57 @@
+import csv
+from time import sleep
+from msedge.selenium_tools import Edge, EdgeOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common import exceptions
+
+
+
+def collect_all_tweets_from_current_view(driver, lookback_limit=25):
+   
+    page_cards = driver.find_elements_by_xpath('//div[@data-testid="tweet"]')
+    if len(page_cards) <= lookback_limit:
+        return page_cards
+    else:
+        return page_cards[-lookback_limit:]
+def extract_data_from_current_tweet_card(card):
+    try:
+        user = card.find_element_by_xpath('.//span').text
+    except exceptions.NoSuchElementException:
+        user = ""
+    except exceptions.StaleElementReferenceException:
+        return
+    try:
+        handle = card.find_element_by_xpath('.//span[contains(text(), "@")]').text
+    except exceptions.NoSuchElementException:
+        handle = ""
+    try:
+        
+        postdate = card.find_element_by_xpath('.//time').get_attribute('datetime')
+    except exceptions.NoSuchElementException:
+        return
+    try:
+        _comment = card.find_element_by_xpath('.//div[2]/div[2]/div[1]').text
+    except exceptions.NoSuchElementException:
+        _comment = ""
+    try:
+        _responding = card.find_element_by_xpath('.//div[2]/div[2]/div[2]').text
+    except exceptions.NoSuchElementException:
+        _responding = ""
+    tweet_text = _comment + _responding
+    try:
+        reply_count = card.find_element_by_xpath('.//div[@data-testid="reply"]').text
+    except exceptions.NoSuchElementException:
+        reply_count = ""
+    try:
+        retweet_count = card.find_element_by_xpath('.//div[@data-testid="retweet"]').text
+    except exceptions.NoSuchElementException:
+        retweet_count = ""
+    try:
+        like_count = card.find_element_by_xpath('.//div[@data-testid="like"]').text
+    except exceptions.NoSuchElementException:
+        like_count = ""
+    tweet = (user, handle, postdate, tweet_text, reply_count, retweet_count, like_count)
+    return tweet
